25d7aced9a3c1920ab013faab51bc44374b470f6
LaurensK <LK@user.noreply.github.com>
2020-11-14 16:08:03 +0100

    Initial work

---
+++ Assets/RushHour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RushHour : MonoBehaviour
+{
+    public bool isActive = true;
+    private CameraMouseMovement _cmm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get Our Camera
+        _cmm = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMouseMovement>();
+        _cmm.Disable_Camera_Movement = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
