14d825c4424604cf3252af5b0ccdc73e830a6991
Romke <17853737+fRomke@users.noreply.github.com>
2020-11-24 15:33:13 +0100

    Initial framework

---
+++ Assets/Scenes/Prototype/Scripts/button_click.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_click : MonoBehaviour
+{
+    public GameObject A;
+    private terminal_main scriptA;
+    private void Start()
+    {
+        A = transform.parent.transform.parent.gameObject;
+        scriptA = A.GetComponent<terminal_main>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+        GameObject progress = GameObject.Find("prog_plane");
+        //Get the Renderer component from the new cube
+        var Renderer = progress.GetComponent<Renderer>();
+
+        //Call SetColor using the shader property name "_Color" and setting the color to red
+        if(scriptA.progressint == 1){
+            Renderer.material.SetColor("_Color", Color.green);
+        }
+        else{
+            Renderer.material.SetColor("_Color", Color.blue);
+        }
+    }
+}
---
+++ Assets/Scenes/Prototype/Scripts/terminal_main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terminal_main : MonoBehaviour
+{
+    public int progressint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        progressint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(progressint);
+    }
+}
