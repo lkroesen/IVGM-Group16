273d07e6e7b1d9a0f40d1513b38bba53d44e0582
Romke <17853737+fRomke@users.noreply.github.com>
2020-11-25 16:32:03 +0100

    Includes door that opens on the correct code.

--- Assets/Scenes/Prototype/Scripts/terminal_main.cs
+++ Assets/Scenes/Prototype/Scripts/terminal_main.cs
@@ -11,19 +11,28 @@ public class terminal_main : MonoBehaviour
     private int correct;
     public bool done;
 
+    //Door
+    private GameObject door;
+    private GameObject hinge;
+    private float angle = 0.0f;
 
     void ChangeColor(GameObject obj, Color c){
+        //Function for changing the color of an object.
         var Renderer = obj.GetComponent<Renderer>();
         Renderer.material.SetColor("_Color", c);
     }
 
     void Start()
     {
+        //Initialising values.
+        door = GameObject.Find("Door");
+        hinge = GameObject.Find("Hinge");
         done = false;
         Fail();
     }
 
     public void ButtonPress(string s){
+        //Handles a button press. Called from button_1.cs.
         if(!done){
             int value = int.Parse(s);
             Debug.Log(value);
@@ -42,12 +51,14 @@ public class terminal_main : MonoBehaviour
     }
 
     void UpdateScreen(){
+        //Turn a new light green.
         string screen = ("screen"+correct.ToString());
         GameObject obj = GameObject.Find(screen);
         ChangeColor(obj, Color.green);
     }
 
     void Fail(){
+        //Reset to 0 and turn all the lights red.
         correct = 0;
         for (int i = 0; i < code.Length; i++){
             string screen = ("screen"+code[i].ToString());
@@ -58,8 +69,18 @@ public class terminal_main : MonoBehaviour
     }
 
     void Succes(){
-        print("Succes");
         done = true;
+    }
+
+    void Update(){
+        //Opening door angle degrees
+        if(done){
+            float delta = 20 * Time.deltaTime;
+            angle = angle + delta;
+            if (angle < 110){
+                door.transform.RotateAround(hinge.transform.position, Vector3.up, delta);
+            }
+        }     
     }
 
     // GameObject screen1;
