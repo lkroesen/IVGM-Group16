866344943acbf2bd113ea00dc1e51e4eb8be8848
LKroesen <lkroesen@user.noreply.github.com>
2020-11-27 17:50:08 +0100

    Add Volume Regulation by Remote Control

---
+++ Assets/VolumeRemote.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeRemote : MonoBehaviour
+{
+    public float mod;
+    public bool clicked;
+    public AudioSource _as;
+
+    private Material _material;
+    private static readonly int Smoothness = Shader.PropertyToID("_Glossiness");
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _material = transform.parent.gameObject.GetComponent<MeshRenderer>().material;
+    }
+    
+    private void OnMouseOver()
+    {
+        _material.SetFloat(Smoothness, 0.409f);
+    }
+    
+    private void OnMouseExit()
+    {
+        _material.SetFloat(Smoothness, 0f);
+    }
+
+    private void OnMouseDown()
+    {
+        clicked = true;
+    }
+
+    private void OnMouseUp()
+    {
+        clicked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clicked)
+        {
+            _as.volume += mod * Time.deltaTime;
+        }
+    }
+}
