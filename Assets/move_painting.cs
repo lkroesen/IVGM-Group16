3281a4fd5b9a93edfb734b59f3ffbc05351d3c58
Romke <17853737+fRomke@users.noreply.github.com>
2020-12-10 18:08:05 +0100

    Safe is now hidden behind a blank painting

---
+++ Assets/move_painting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_painting : MonoBehaviour
+{
+    static private bool slide;
+    static float distance = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slide = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float delta = Time.deltaTime /4;
+        if (slide){
+            distance = distance + delta;
+            if(distance < 1)
+                transform.Translate(new Vector3(delta,0, 0));
+        }
+    }
+
+    private void OnMouseDown() {
+        slide = true;
+    }
+}
